86be182cb794e52a79cc898df9c255b6ca71aa24
Drizzt <vbcc2001@gmail.com>
2025-01-18 18:21:59 +0800

    1

--- packages/utils/dist/utils.es.js
+++
@@ -1,5 +0,0 @@
-const o = (t, c) => t + c, s = (t, c) => t - c;
-export {
-  o as add,
-  s as subtract
-};
--- packages/utils/dist/utils.umd.js
+++
@@ -1,1 +0,0 @@
-(function(e,t){typeof exports=="object"&&typeof module<"u"?t(exports):typeof define=="function"&&define.amd?define(["exports"],t):(e=typeof globalThis<"u"?globalThis:e||self,t(e.utils={}))})(this,function(e){"use strict";const t=(n,d)=>n+d,i=(n,d)=>n-d;e.add=t,e.subtract=i,Object.defineProperty(e,Symbol.toStringTag,{value:"Module"})});
