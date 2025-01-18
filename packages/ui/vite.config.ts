ef7756d4d4dbafc4e8c4bb9e2b854b9c8d2e4b29
Drizzt <vbcc2001@gmail.com>
2025-01-18 21:59:36 +0800

    1

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -5,9 +5,19 @@ export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: './src/index.ts',  // 模块的入口文件
-      name: 'utils',  // 你希望导出的模块名称
+      entry: './src/index.tsx',  // 模块的入口文件
+      name: 'ui',  // 你希望导出的模块名称
       fileName: (format) => `ui.${format}.js`,  // 输出文件名格式
     },
+    rollupOptions: {
+      // 确保 external 将 React 和 React DOM 排除在外
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
   },
 })
