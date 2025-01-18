5ab9b012a422eb6148ffa8b98bc9a7123a22510e
Drizzt <vbcc2001@gmail.com>
2025-01-18 18:40:06 +0800

    1

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/index.ts',  // 模块的入口文件
+      name: 'utils',  // 你希望导出的模块名称
+      fileName: (format) => `ui.${format}.js`,  // 输出文件名格式
+    },
+  },
 })
