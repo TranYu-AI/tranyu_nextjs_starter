549dfdab9a36e2d71c52bb8838ff57ed1b8fc4a3
Drizzt <vbcc2001@gmail.com>
2025-01-18 17:22:53 +0800

    1

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -8,15 +8,5 @@ export default defineConfig({
       name: 'utils',  // 你希望导出的模块名称
       fileName: (format) => `utils.${format}.js`,  // 输出文件名格式
     },
-    rollupOptions: {
-      // 确保外部依赖不被打包
-      external: ['react'],  // 如果你的 utils 依赖于 React，类似的外部依赖可以放在这里
-      output: {
-        // 设置全局变量（如 UMD 格式的情况下）
-        globals: {
-          react: 'React',
-        },
-      },
-    },
   },
 });
